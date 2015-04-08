9775e0bf9202fd96481db5a03588844a96a9bbfa
InfamousSYN <ainfamoussyn@gmail.com>
2015-04-08 22:11:13 +1000

    Added GDB to software install attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@
 default["libraries"]["i386"] = ["libc6:i386","libncurses5:i386","libstdc++6:i386"]
 
 # Attributes for software recipes
-default["software"]["software"] = ["gcc-multilib"]
+default["software"]["software"] = ["gcc-multilib", "gdb"]
