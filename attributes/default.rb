dae3fd9a9dd2c6af11e821988b916f111f073ede
InfamousSYN <ainfamoussyn@gmail.com>
2015-06-01 21:26:01 +1000

    Fix missing pwntools dependency

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,13 +10,13 @@ default['libraries']['i386']			= ['libc6:i386','libncurses5:i386','libstdc++6:i3
 
 # Attributes for applications
 default['applications']['packages']		= ['gcc-multilib','gdb','unzip','git']
-default['ida']['enabled']				= false
+default['ida']['enabled']				= true
 default['ida']['package']				= 'idademo68_linux.tgz'
-default['hopper']['enabled']			= false
+default['hopper']['enabled']			= true
 default['hopper']['package']			= 'hopperv3-3.9.5.deb'
 default['hopper']['depends']			= ['libavahi-common3','libavahi-client3','libxslt1.1','libpthread-workqueue0','libkqueue0','libblocksruntime0','libqt5core5a','libqt5gui5','libqt5network5','libqt5printsupport5','libqt5widgets5','libqt5xml5']
 default['hopper']['dir']				= '/opt'
-default['pwntools']['enabled']			= false
+default['pwntools']['enabled']			= true
 #default['pwntools']['latest']			= 'git+https://github.com/Gallopsled/pwntools#egg=pwntools'
 default['pwntools']['package']			= 'pwntools'
-default['pwntools']['depends']			= ['python-pip','binutils-aarch64-linux-gnu']
+default['pwntools']['depends']			= ['python-dev','python-pip','binutils-aarch64-linux-gnu']
